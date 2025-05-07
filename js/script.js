ac003695558dfca6e214b1c2e3ebb76f64949c72
wesleyyach <wesleymaingenji@gail.com>
2025-05-07 14:41:59 -0300

    Primeiro commit: Calculadora web funcional

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLastChar() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Erro';
+        setTimeout(clearDisplay, 1000);
+    }
+} 
